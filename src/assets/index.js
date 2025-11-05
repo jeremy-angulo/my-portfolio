1900e0f982e06ffe1d0f809b162a3c6cf124fe38
Skysynth <skysynth31@gmail.com>
2025-11-05 21:31:44 +0100

    Vite js optimisation for Vercel

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import logo from "./Logo_JA.png";
+import jeremy from "./profile_picture.png";
+
+import github from "./github.png";
+import demo from "./demo.png"
+
+import ltu from "./LTU_Logo.png";
+import ltu_square from "./LTU_Logo_Square.png";
+import enseeiht from "./ENSEEIHT_Logo.png";
+import paulsab_squared from "./Logo_PaulSab_Squared.png";
+import paulsab from "./Logo_PaulSab.png";
+import thesis from "./Thesis.png";
+
+import alten from "./Logo_Alten.png";
+import boost from "./Logo_Boost.png";
+import presentation_boost from "./Presentation_Boost.png";
+import cnes from "./Logo_CNES.png";
+import cnes_white from "./Logo_CNES_White.png";
+import bocalenvers from "./Logo_Bocalenvers.png";
+import continentphone from "./Logo_ContinentPhone.png";
+import presentation_continentphone from "./Presentation_ContinentPhone.png";
+
+import n7consulting from "./Logo_N7C.png";
+import storizzz from "./Logo_Storizzz.png";
+import zzz from "./Logo_zzz.png";
+import presentation_storizzz from "./Presentation_Storizzz.png";
+import klatterhuset from "./Logo_Klatterhuset.png";
+import man3d from "./Digital_Machine.png";
+import vinci from "./Logo_Vinci.png";
+
+import freelance from "./profile_picture_bg.png";
+import n7chess from "./Logo_N7etMat.png";
+import grinp from "./Logo_GRINP.png";
+import bafa from "./Logo_BAFA_V2.png";
+import presentation_BAFA from "./Presentation_BAFA.png";
+
+export {
+  logo,
+  jeremy,
+  github,
+  demo,
+  ltu,
+  ltu_square,
+  enseeiht,
+  paulsab_squared,
+  paulsab,
+  thesis,
+  alten,
+  boost,
+  presentation_boost,
+  cnes,
+  cnes_white,
+  bocalenvers,
+  continentphone,
+  presentation_continentphone,
+  n7consulting,
+  storizzz,
+  zzz,
+  presentation_storizzz,
+  klatterhuset,
+  man3d,
+  vinci,
+  freelance,
+  n7chess,
+  grinp,
+  bafa,
+  presentation_BAFA,
+}
